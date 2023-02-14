c946bccee63b812250cb223ded8475686974db51
Flexi <flexifailix@gmail.com>
2023-02-14 17:13:17 +0100

    Doing execise 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,44 +1,55 @@
 const Header = (props) => {
   return (
-    <h1>{props.courseName}</h1>
+    <h1>{props.course}</h1>
   );
 };
 
 const Content = (props) => {
   return (
     <>
-      <Part part={props.partList[0]} />
-      <Part part={props.partList[1]} />
-      <Part part={props.partList[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   );
 };
 
 const Part = (props) => {
   return (
-    <p>{props.part.name} {props.part.numberOfExercise}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   );
 };
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.partList[0].numberOfExercise + props.partList[1].numberOfExercise + props.partList[2].numberOfExercise}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   );
 };
 
 const App = () => {
-  const courseName = 'Half Stack application development';
-  const partList = [
-    { name: 'Fundamentals of React', numberOfExercise: 10 },
-    { name: 'Using props to pass data', numberOfExercise: 7 },
-    { name: 'State of a component', numberOfExercise: 14 }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header courseName={courseName} />
-      <Content partList={partList} />
-      <Total partList={partList} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
