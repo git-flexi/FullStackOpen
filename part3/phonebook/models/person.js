b9a59d0533c10e6cd4e4a0ff941737d8b7ba6a69
Flexi <flexifailix@gmail.com>
2023-02-26 17:26:30 +0100

    Removing console.log cause of password

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const url = process.env.MONGODB_URI;
 
-console.log('Connecting to', url);
-
 mongoose.connect(url)
     .then(result => {
         console.log('Connected to MongoDB');
