7dbb8c90cd86f537a0dedfa11c43388325235401
Flexi <flexifailix@gmail.com>
2023-03-18 16:48:04 +0100

    Addinx exercises 7.4-7.6

--- part7/routed-anecdotes/README.md
+++ part7/routed-anecdotes/README.md
@@ -1,2 +1,4 @@
-# Solutions for Exercise 7.1-7.3
+# Solutions for Exercise 7.1-7.6
 https://fullstackopen.com/en/part7/react_router#exercises-7-1-7-3
+
+https://fullstackopen.com/en/part7/custom_hooks#exercises-7-4-7-8
--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useField } from './hooks';
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom';
 
 const Menu = () => {
@@ -62,19 +63,26 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const { reset: resetContent, ...content } = useField('content');
+  const { reset: resetAuthor, ...author } = useField('author');
+  const { reset: resetInfo, ...info } = useField('info');
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
+  };
+
+  const reset = (e) => {
+    e.preventDefault();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
@@ -83,17 +91,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={reset}>reset</button>
       </form>
     </div>
   );
